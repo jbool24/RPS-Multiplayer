cb890e016d36852eb1127dfdbeccca22b20a39bc
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2017-01-10 19:37:37 -0500

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RPS-Multiplayer
 Rock-Paper-Scissor Game -- Multiplayer Version
+
+The game is simple. Sign in with the credentials given by your opponent.
---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+$().ready(function(){
+
+  var config = {
+    apiKey: "AIzaSyAYUAaY5jtLg15tJ7FC-54Rjet9si8YCmg",
+    authDomain: "rpsmultiplayer-cba54.firebaseapp.com",
+    databaseURL: "https://rpsmultiplayer-cba54.firebaseio.com",
+    storageBucket: "rpsmultiplayer-cba54.appspot.com",
+    messagingSenderId: "492026812941"
+  };
+
+  firebase.initializeApp(config);
+
+
+}); //--end
